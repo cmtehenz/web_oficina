414e4d237c56b0354d885b7cc3b4f4681071d898
Gustavo Costa <cmtehenz@gmail.com>
2021-07-02 21:09:54 -0300

    :tada: _Configurando React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,25 +12,35 @@ import {
   Th,
   Td,
   Checkbox,
+  Spinner,
   useBreakpointValue,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from 'react-query';
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
+
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    return data;
+  });
+
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data))
+
   },[]);
 
   return (
@@ -57,7 +67,17 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ): error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ): (
+            <>
+            <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
                 <Th px={["4", "4", "6"]} color="gray.600" width="8">
@@ -174,8 +194,13 @@ export default function UserList() {
                 </Td>
               </Tr>
             </Tbody>
-          </Table>
-          <Pagination />
+
+            </Table>
+            <Pagination />
+            </>
+          )}
+
+
         </Box>
       </Flex>
     </Box>
