6927729de9df2e5429ecf397a96d505e69933251
Gustavo Costa <cmtehenz@gmail.com>
2021-07-07 10:20:23 -0300

    :tada: _Prefetch de dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,15 +14,19 @@ import {
   Checkbox,
   Spinner,
   useBreakpointValue,
+  Link,
 } from "@chakra-ui/react";
-import Link from "next/link";
+import NextLink from "next/link";
 import { useState } from "react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine, RiPencilLine, RiUserReceived2Fill } from "react-icons/ri";
+import { QueryClient } from "react-query";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import api from "../../services/api";
 import { useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
 
 export default function UserList() {
@@ -33,6 +37,16 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+  async function handlePrefetchUser(userId: number){
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`);
+
+      return response.data;
+    }, {
+      staleTime: 1000 * 60 * 10, // 10 minutos
+    })
+  }
 
   return (
     <Box>
@@ -46,7 +60,7 @@ export default function UserList() {
               Usuários
               { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
             </Heading>
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -56,7 +70,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
           { isLoading ? (
@@ -90,7 +104,9 @@ export default function UserList() {
                     </Td>
                 <Td>
                   <Box>
-                    <Text fontWeight="bold">{user.name}</Text>
+                    <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(Number(user.id))} >
+                      <Text fontWeight="bold">{user.name}</Text>
+                    </Link>
                     <Text fontSize="small" color="gray.300">
                       {user.email}
                     </Text>
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -162,7 +162,7 @@ export function makeServer(){
         )
 
       });
-
+      this.get('users/:id');
       this.post('/users');
 
       this.get('/logbooks', function(schema, request){
