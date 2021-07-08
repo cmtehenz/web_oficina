88d307873aeb9eb15faba36b352d551c8bf54984
Gustavo Costa <cmtehenz@gmail.com>
2021-07-08 20:01:04 -0300

    :tada: _Salvando dados de usuários

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 interface ProfileProps {
   showProfileData: Boolean;
 }
 
 export function Profile({ showProfileData = true }: ProfileProps) {
+  const { user } = useContext(AuthContext);
+
   return (
     <Flex align="center">
       {showProfileData && (
         <Box mr="4" textAlign="right">
           <Text>Gustavo Altevir da Costa</Text>
           <Text color="gray.300" fontSize="small">
-            cmtehenz@gmail.com
+            {user?.email}
           </Text>
         </Box>
       )}
